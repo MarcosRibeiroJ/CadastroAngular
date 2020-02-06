266558c9a4336ffd2470f9fa692d72167c5d32f9
Marcos Ribeiro <marcossrj90@gmail.com>
2020-02-06 20:29:34 -0300

    adicionando menu com links para as páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { FornecedoresComponent } from './fornecedores/fornecedores.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path:'home', component: HomeComponent },
+  { path:'clientes', component: ClientesComponent },
+  { path:'fornecedores', component: FornecedoresComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { FornecedoresComponent } from './fornecedores/fornecedores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClientesComponent,
+    FornecedoresComponent
   ],
   imports: [
     BrowserModule,
